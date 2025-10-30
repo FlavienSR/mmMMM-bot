83f83d3e78db43c86263aae446c3a6a93dc429d1
FlavienSR <flavienjh@gmail.com>
2025-10-30 12:07:47 +0100

    feat: add rmd slash command

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -1,3 +1,7 @@
+import re
+import asyncio
+from datetime import datetime, timedelta
+
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -24,8 +28,37 @@ class SlashCommands(commands.Cog):
             color=discord.Color.light_gray()
         )
         embed.add_field(name="/ping", value="Pour test le bot + latence", inline=False)
+        embed.add_field(name="/rmd", value="Pour ajouter un rappel", inline=False)
         embed.add_field(name="/help", value="Affiche ce message d’aide.", inline=False)
         await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="rmd", description="Pour avoir un reminder")
+    @app_commands.guilds(discord.Object(id=PERSO),discord.Object(id=ROOTFARM))
+    async def slash_rmd(self, interaction: discord.Interaction, temps: str, raison: str):
+        pattern_date = re.compile(r"\b(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[0-2])[\/\-](\d{2}|\d{4})\s+(?:[01]?\d|2[0-3]):[0-5]\d(?:\:[0-5]\d)?\b", re.IGNORECASE)
+        pattern_temps = re.compile(r"^(?:(\d+)j)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?$", re.IGNORECASE)
+        if pattern_date.search(temps):
+            target = datetime.strptime(temps, "%d/%m/%Y %H:%M")
+            now = datetime.now()
+            delay = (target - now).total_seconds()
+            if delay <= 0:
+                await interaction.response.send_message(f"Il faut mettre une date dans le futur!")
+            else:
+                await interaction.response.send_message(f"Rappel ajouté ({temps}). Raison : {raison}")
+                await asyncio.sleep(delay)
+                await interaction.user.send(f"RAPPEL : {raison}")
+        elif pattern_temps.search(temps):
+            match = pattern_temps.match(temps)
+            jours, heures, minutes, secondes = match.groups(default="0")
+            total = (int(jours) * 86400 + int(heures) * 3600 + int(minutes) * 60 + int(secondes))
+            if total <= 0:
+                await interaction.response.send_message(f"Il faut mettre un temps supérieur à 0!")
+            else:
+                await interaction.response.send_message(f"Rappel ajouté (dans {temps}). Raison : {raison}")
+                await asyncio.sleep(total)
+                await interaction.user.send(f"RAPPEL : {raison}")
+        else:
+            await interaction.response.send_message(f"Mauvais formattage. (Rappel: jj/mm/aaaa hh:mm OU XXj/h/m/s")
 
 async def setup(bot):
     await bot.add_cog(SlashCommands(bot))
