e433587068d41f64a7f054cd3a957e4a01f00a5d
Flavien Jobard-Houdusse <flavien.jobard-houdusse@infomaniak.com>
2025-11-06 13:47:11 +0100

    fix: rm keep_alive

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
-from keep_alive import keep_alive
 
 
 load_dotenv()
@@ -12,8 +11,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-keep_alive()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
